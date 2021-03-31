a3043741b5fdd8e95e3b0641dc8f50571e27c861
Donghan Jiang <2641635339@qq.com>
2021-03-31 15:46:10 +0800

    feat: add github actions workflow

--- packages/hospital-backend/configs/environments/production.ts
+++ packages/hospital-backend/configs/environments/production.ts
@@ -1,12 +1,10 @@
 export default {
   identity: 'production',
-
-  port: 5999,
-
-  DB_TYPE: 'mongodb',
-  MY_HOST: '',
-  MY_PORT: '',
-  USERNAME: '',
-  PASSWORD: '',
-  DATABASE: '',
+  port: 5099,
+  DB_TYPE: 'mysql',
+  MY_HOST: 'localhost',
+  MY_PORT: 3306,
+  USERNAME: 'root',
+  PASSWORD: '123456',
+  DATABASE: 'hospital',
 }
