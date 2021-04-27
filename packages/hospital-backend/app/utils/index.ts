f2988f89356389b51ae27f31c2522d756a642104
Donghan Jiang <2641635339@qq.com>
2021-04-27 22:43:42 +0800

    fix: password encrypt bug fix

--- packages/hospital-backend/app/utils/index.ts
+++ packages/hospital-backend/app/utils/index.ts
@@ -2,6 +2,6 @@ const crypto = require('crypto')
 
 export const encryptPassword = (password: string) => {
   let md5 = crypto.createHash('md5')
-  let md5Sum = md5.update('hello')
+  let md5Sum = md5.update(password)
   return md5Sum.digest('Base64')
 }
